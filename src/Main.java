2721de87136fd10ccc41f34e9afad55f8cc5506b
vkray <vitalikkrainyuk@yandex.ru>
2023-04-12 14:22:57 +0300

    hw1. Условный оператор. Урок 2. Задача 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        int iOs = 3;
+        boolean clientOS = iOs == 0;
+        if (clientOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+    }
+}
