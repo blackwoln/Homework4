75cfdc9f932124f93a87f62189ffe6372ee7fb98
vkray <vitalikkrainyuk@yandex.ru>
2023-04-12 16:27:50 +0300

    hw2. Условный оператор. Урок 2. Задача 2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,41 @@ public class Main {
         task1();
         task2();
     }
-
     public static void task1 () {
         System.out.println("Задача 1");
 
         int iOs = 3;
-        boolean clientOS = iOs == 0;
-        if (clientOS) {
+        int Android = 1;
+        if (iOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
+        } if (Android == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
 
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+
+        int iOs = 1;
+        int Android = 1;
+        int date = 2015;
+        int dateTelephone = 2013;
+        boolean dateIOsTelephoneNew = iOs == 0 && dateTelephone >= date;
+        boolean dateIOsTelephoneStar = iOs == 0 && dateTelephone < date;
+        boolean dateAndroidTelephoneNew = Android == 1 && dateTelephone >= date;
+        boolean dateAndroidTelephoneStar = Android == 1 && dateTelephone < date;
+        if (dateIOsTelephoneNew) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (dateAndroidTelephoneNew) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else  if (dateIOsTelephoneStar) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (dateAndroidTelephoneStar) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
     }
 }
