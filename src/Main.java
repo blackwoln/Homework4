e90e51750fc9da57dea8cdc21de9c20b25c02eff
vkray <vitalikkrainyuk@yandex.ru>
2023-04-13 11:33:44 +0300

    hw3. Условный оператор. Урок 2. Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -39,5 +42,28 @@ public class Main {
         }
 
 
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+        int year = 2004;
+        if ( year % 4 == 0 && year % 100 == 0 && year % 400 == 0 ) {
+            System.out.println( + year + " год является високосным");
+        } else if (year % 4 == 0 && year % 100 != 0 ) {
+            System.out.println(+ year + " год является високосным");
+        } else
+        {
+            System.out.println(+ year + " год не является високосным");
+        }
+        
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
     }
 }
